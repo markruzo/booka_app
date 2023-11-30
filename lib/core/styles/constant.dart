26284af474f0aabf567aef070b6ca7e12547139d
Mario Krutzikowsky <135688240+markruzo@users.noreply.github.com>
2023-11-30 07:42:28 +0100

    Typography

---
+++ lib/core/styles/constant.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  // Dark Mode Colors
+  static const Color kColorFluffy = Color.fromARGB(255, 32, 32, 32);
+  static const Color kColorWater = Color.fromARGB(255, 50, 68, 234);
+  static const Color colorWaterOpacity = Color.fromARGB(255, 32, 32, 32);
+  static const Color kColorClean = Color.fromARGB(255, 20, 20, 20);
+  static const Color kColorBlack = Color.fromARGB(255, 255, 255, 255);
+  static const Color kColorTropical = Color.fromARGB(255, 20, 34, 36);
+  static const Color ckColorOcean = Color.fromARGB(255, 15, 25, 63);
+  static const Color kColorActive = Color.fromARGB(255, 15, 15, 15);
+  static const Color kColorActiveIcon = Color.fromARGB(255, 47, 204, 113);
+  static const Color kColorOffline = Color.fromARGB(155, 32, 32, 32);
+  static const Color kColorOfflineIcon = Color.fromARGB(255, 107, 114, 128);
+  static const Color kColorRequested = Color.fromARGB(255, 40, 40, 40);
+  static const Color kColorRequestedIcon = Color.fromARGB(255, 251, 126, 17);
+  static const Color kColorInTransit = Color.fromARGB(255, 25, 25, 25);
+  static const Color kColorInTransitIcon = Color.fromARGB(255, 224, 151, 206);
+  static const Color kColorLaundry = Color.fromARGB(255, 30, 30, 30);
+  static const Color kColorLaundryIcon = Color.fromARGB(255, 50, 68, 234);
+  static const Color kColorAlertIcon = Color.fromRGBO(255, 82, 82, 1);
+  static const Color kColorDisabled = Color.fromARGB(255, 40, 40, 40);
+
+  // Light Mode Colors
+  static const Color kLightModeColorFluffy = Color.fromARGB(255, 255, 229, 248);
+  static const Color kLightModeColorWater = Color.fromARGB(255, 50, 68, 234);
+  static const Color kLightModeColorWaterOpacity =
+      Color.fromARGB(255, 192, 197, 248);
+  static const Color kLightModeColorClean = Color.fromARGB(255, 252, 252, 252);
+  static const Color kLightModeColorBlack = Color.fromARGB(255, 27, 28, 34);
+  static const Color kLightModeColorTropical =
+      Color.fromARGB(255, 190, 236, 250);
+  static const Color kLightModeCkColorOcean = Color.fromARGB(255, 35, 56, 138);
+  static const Color kLightModeColorActive = Color.fromARGB(255, 234, 250, 241);
+  static const Color kLightModeColorActiveIcon =
+      Color.fromARGB(255, 47, 204, 113);
+  static const Color kLightModeColorOffline =
+      Color.fromARGB(155, 241, 241, 242);
+  static const Color kLightModeColorOfflineIcon =
+      Color.fromARGB(255, 107, 114, 128);
+  static const Color kLightModeColorRequested =
+      Color.fromARGB(255, 255, 242, 231);
+  static const Color kLightModeColorRequestedIcon =
+      Color.fromARGB(255, 251, 126, 17);
+  static const Color kLightModeColorInTransit =
+      Color.fromARGB(255, 255, 238, 250);
+  static const Color kLightModeColorInTransitIcon =
+      Color.fromARGB(255, 224, 151, 206);
+  static const Color kLightModeColorLaundry =
+      Color.fromARGB(255, 235, 236, 253);
+  static const Color kLightModeColorLaundryIcon =
+      Color.fromARGB(255, 50, 68, 234);
+  static const Color kLightModeColorAlertIcon = Color.fromRGBO(255, 82, 82, 1);
+  static const Color kLightModeColorDisabled =
+      Color.fromARGB(255, 240, 241, 242);
+
+  // DM Sans Typography Styles
+  static const TextStyle kBodyLargeStyle =
+      TextStyle(fontFamily: 'Dm Sans', fontSize: 18);
+  static const TextStyle kBodyMediumStyle =
+      TextStyle(fontFamily: 'Dm Sans', fontSize: 16);
+  static const TextStyle kBodySmallStyle =
+      TextStyle(fontFamily: 'Dm Sans', fontSize: 14);
+  static const TextStyle kBodyExtraSmallStyle =
+      TextStyle(fontFamily: 'Dm Sans', fontSize: 12);
+  static const TextStyle kCaptionStyle =
+      TextStyle(fontFamily: 'Dm Sans', fontSize: 10);
+  static const TextStyle kOverlineStyle =
+      TextStyle(fontFamily: 'Dm Sans', fontSize: 8);
+
+  // Gustavo Typography Styles
+  static const TextStyle kDisplayLargeStyle =
+      TextStyle(fontFamily: 'Gustavo', fontSize: 26);
+  static const TextStyle kDisplayMediumStyle =
+      TextStyle(fontFamily: 'Gustavo', fontSize: 20);
+}
